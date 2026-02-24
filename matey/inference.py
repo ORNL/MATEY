b140104737707a5e86bdcabf06289d2d0ac5a98d
Andrey Prokopenko <prokopenkoav@ornl.gov>
2026-02-24 11:13:26 -0800

    Update inference

--- matey/inference.py
+++ matey/inference.py
@@ -147,6 +147,13 @@ class Inferencer:
             else:
                 cond_input = None
 
+            if "geometry" in data:
+                geometry = data["geometry"]
+                geometry["geometry"] = geometry["geometry"].to(self.device)
+            else:
+                geometry = None
+
+
             cond_dict = {}
             try:
                 cond_dict["labels"] = data["cond_field_labels"].to(self.device)
@@ -167,12 +174,17 @@ class Inferencer:
                     tkhead_type = 'graph'
                     inp = graphdata
                     imod_bottom = imod
+                elif "geometry" in data:
+                    inp = rearrange(inp.to(self.device), 'b t c d h w -> t b c d h w')
+                    tkhead_type = 'gno'
+                    inp = (inp, geometry)
+                    imod_bottom = -1 # not used
                 else:
                     inp = rearrange(inp.to(self.device), 'b t c d h w -> t b c d h w')
                     tkhead_type = 'default'
                     imod_bottom = determine_turt_levels(self.model.module.tokenizer_heads_params[tkhead_name][-1], inp.shape[-3:], imod) if imod>0 else 0
                 seq_group = self.current_group if dset_type in self.valid_dataset.DP_dsets else None
-                print(f"Rank {self.global_rank} input shape {inp.shape if tkhead_type == 'default' else inp}, dset_type {dset_type}", flush=True)
+                print(f"Rank {self.global_rank} input shape {inp.shape if tkhead_type == 'default' else inp[0].shape if tkhead_type == 'gno' else inp}, dset_type {dset_type}", flush=True)
                 opts = ForwardOptionsBase(
                 imod=imod, 
                 imod_bottom=imod_bottom,
